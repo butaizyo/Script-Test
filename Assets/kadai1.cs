fa0ef9927566e980b890331d480fcda9342789cb
岡遼 <oka1119ryo@gmail.com>
2019-01-20 09:20:17 +0900

    修正したスクリプトです。

---
+++ Assets/kadai1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kadai1 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		//要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化してください。
+		int[] array = { 1, 3, 5, 7, 9 };
+
+		//for文を使い、配列の各要素の値を順番に表示してください
+		for (int i = 0; i < array.Length; i++) {
+
+
+			Debug.Log (array [i]);
+		}
+		for (int i = array.Length - 1; 0 <= i; i--) {
+
+			Debug.Log (array [i]);
+		}
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
---
+++ Assets/kadai2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai2 : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Magicクラスの変数を宣言しインスタンスを代入
+		Boss b = new Boss ();
+
+		//マジック関数を呼び出し
+
+		//11回繰り返し
+		for (int i = 0; i < 11; i++) {
+			b.Magic (5);
+
+		}
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
+
+public class Boss
+{
+	private int mp = 53;
+	//MP
+
+	//残りMP用の関数
+	public void Magic (int a)
+	{ 
+		if (mp >= a) {
+			mp -= a;
+			Debug.Log ("魔法攻撃をした。残りMPは" + mp);
+		} else {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		}
+	}
+}
